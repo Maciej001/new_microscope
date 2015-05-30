a4cd947b7158e7eb13709356f14084e3470e81ea
Maciej Nowakowski <m.nowakowskipl@gmail.com>
2015-05-30 13:48:50 +0100

    active header class

--- client/templates/includes/header.js
+++ client/templates/includes/header.js
@@ -1,7 +1,7 @@
 Template.header.helpers({
 	activeRouteClass: function(/* route names */) {
 		var args = Array.prototype.slice.call(arguments, 0);
-		args.pop();
+		args.pop(); // removes hash added at the end by Spacebars
 
 		var active = _.any(args, function(name){
 			return Router.current() && Router.current().route.getName() === name
